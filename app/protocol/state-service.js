b3b5199e33442f73960288efc3325fd1323dd161
Kiril Knysh <k.knysh@gmail.com>
2017-03-07 21:45:08 +0100

    Remove params validation;

--- app/protocol/state-service.js
+++ app/protocol/state-service.js
@@ -9,17 +9,9 @@ const MESSAGE_NAME = {
 exports.MESSAGE_NAME = MESSAGE_NAME;
 
 exports.sessionJoin = function (sessionId, participantId) {
-    if (!sessionId || !participantId) {
-        throw new TypeError('sessionId, participantId are required for session.join message');
-    }
-
     return createMessage(identity, { name: MESSAGE_NAME.sessionJoin, sessionId, participantId });
 };
 
 exports.sessionLeave = function (sessionId, participantId) {
-    if (!sessionId || !participantId) {
-        throw new TypeError('sessionId, participantId are required for session.leave message');
-    }
-
     return createMessage(identity, { name: MESSAGE_NAME.sessionLeave, sessionId, participantId });
 };
--- app/protocol/ui.js
+++ app/protocol/ui.js
@@ -9,17 +9,9 @@ const MESSAGE_NAME = {
 exports.MESSAGE_NAME = MESSAGE_NAME;
 
 exports.participantJoined = function (sessionId, participantId, displayName) {
-    if (!sessionId || !participantId || !displayName) {
-        throw new TypeError('sessionId, participantId, displayName are required for participant.joined message');
-    }
-
     return createMessage(identity, { name: MESSAGE_NAME.participantJoined, sessionId, participantId, displayName });
 };
 
 exports.participantLeft = function (sessionId, participantId) {
-    if (!sessionId || !participantId) {
-        throw new TypeError('sessionId, participantId are required for participant.left message');
-    }
-
     return createMessage(identity, { name: MESSAGE_NAME.left, sessionId, participantId });
 };
