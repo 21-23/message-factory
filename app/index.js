ee8ff24d1478986b3e9f585399a25ec56150a7b8
Kiril Knysh <k.knysh@gmail.com>
2017-03-11 15:15:25 +0100

    ⚒ 🏭

--- app/index.js
+++ app/index.js
@@ -1,14 +1,19 @@
+// TODO: move all messages to protocol; forbid to use createMessage
 const { createMessage, parseMessage } = require('./message-factory');
 
 const arnaux = require('./arnaux-messages');
-const uiProtocol = require('./protocol/ui');
+const frontServiceProtocol = require('./protocol/front-service');
+const sandboxServiceProtocol = require('./protocol/sandbox-service');
 const stateServiceProtocol = require('./protocol/state-service');
+const uiProtocol = require('./protocol/ui');
 
 module.exports = {
     createMessage,
     parseMessage,
     arnaux,
     protocol: {
+        frontService: frontServiceProtocol,
+        sandboxService: sandboxServiceProtocol,
         stateService: stateServiceProtocol,
         ui: uiProtocol,
     }
