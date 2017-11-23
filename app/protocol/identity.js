a998166606587547c477245832424467b54475ab
Kiril Knysh <k.knysh@gmail.com>
2017-11-23 18:46:27 +0100

    Fix export;

--- app/protocol/identity.js
+++ app/protocol/identity.js
@@ -1,8 +1,8 @@
-exports = {
+module.exports = {
     MESSENGER: 'messenger',
     FRONT_SERVICE: 'front-service',
     INIT_SERVICE: 'init-service',
     SANDBOX_SERVICE: 'sandbox-service',
     STATE_SERVICE: 'state-service',
-    UI: 'ui'
+    UI: 'ui',
 };
